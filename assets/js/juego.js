22ec40894fff473af149769e6ce8b38dd7e8b6ab
Sebastian Gonzalez <smgonalez@gmail.com>
2025-04-11 11:43:17 +0200

    pedir carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -25,14 +25,25 @@ const crearDeck = () => {
             deck.push( esp + tipo);
         }
     }
-
-    console.log( deck );// puesh es para meter cosas a aun array
+    // push es para meter cosas a aun array
     deck = _.shuffle ( deck );
     console.log( deck );
-    
+    return deck;
+
 }
 
 crearDeck();
 
+//funcion para tomar una carta 
+const pedirCarta = () => {
+    if( deck.length === 0 ) {
+        throw 'no hay mas cartas en el deck';
+    }
 
+    let carta = deck.pop();
+        console.log( deck ); // carta debe ser de la baraja y dejar de existir en el array
+        console.log( carta ); 
+    return;
+}
 
+//pedirCarta()
